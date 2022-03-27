60c2d7982f304049e9c795442fe97c90c6cffdc1
blueishot <fabricioteixeirahungria@gmail.com>
2022-03-27 01:45:30 -0300

    Login Remake, JS, CSS and HTML

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const labels = document.querySelectorAll('.loginInput label');
+function logar() {
+    let user = window.document.getElementsByClassName('inputText')[0]
+    let pass = window.document.getElementsByClassName('inputPassword')[0]
 
-labels.forEach(label => {
-    labels.innerHTML = label.innerText
-    .split('')
-    .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>` )
-    .join('')
-})
+    if (user.value.length == 0 || pass.value.length ==0) {
+        window.alert('Por favor verifique as credenciais')
+    } else {
+        window.alert('Você digitou qualquer coisa mas o dev não fez nada quanto a isso.')
+    }
+}
+
+function redesocial() {
+    window.alert('Você tentou logar por alguma rede social')
+}
+
+function forgotten() {
+    window.alert('O dev não sabe recuperar conta')
+}
+
+function criaConta() {
+    window.alert('O dev não fez página de registro')
+}
