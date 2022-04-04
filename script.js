a656e6527c4d14fd506e56c0674538e478a72816
blueishot <fabricioteixeirahungria@gmail.com>
2022-04-03 22:39:04 -0300

    Fail login

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-function logar() {
-    let user = window.document.getElementsByClassName('inputText')[0]
-    let pass = window.document.getElementsByClassName('inputPassword')[0]
+let user = window.document.getElementsByClassName('inputText')[0]
+let pass = window.document.getElementsByClassName('inputPassword')[0]
+let loginInput = window.document.getElementsByClassName('loginInput')[0]
+let passwordInput = window.document.getElementsByClassName('loginInput')[1]
+let button = window.document.getElementsByClassName('loginButton')[0]
 
-    if (user.value.length == 0 || pass.value.length ==0) {
+function logar() {
+    if (user.value.length == 0) {
         window.alert('Por favor verifique as credenciais')
+        loginInput.style.border = 'red 1px solid'
+        passwordInput.style.border = 'none'
+    } else if (pass.value.length == 0) {
+        window.alert('Por favor verifique as credenciais')
+        passwordInput.style.border = 'red 1px solid'
+        loginInput.style.border = 'none'
     } else {
         window.alert('Você digitou qualquer coisa mas o dev não fez nada quanto a isso.')
+        loginInput.style.border = 'none'
+        passwordInput.style.border = 'none'
     }
+  
+}
+
+function loginBorder() {
+    loginInput.style.border = 'none'
+}
+
+function passBorder() {
+    passwordInput.style.border = 'none'
 }
 
 function redesocial() {
